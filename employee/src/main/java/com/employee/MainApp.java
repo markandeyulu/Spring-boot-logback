2fbc08188ca60e47cda5752f9bfcfeade817d1c3
Markandeyulu Kothapu <markandeyulu.kothapu@gmail.com>
2021-06-22 20:14:02 -0400

    init commit

---
+++ employee/src/main/java/com/employee/MainApp.java
@@ -0,0 +1,17 @@
+package com.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan
+public class MainApp {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(MainApp.class, args);
+
+	}
+	
+}
